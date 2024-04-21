92393ecabf36e71c891b74f92b34849810100bfb
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-04-21 11:55:26 +0200

    working with traits & imports

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,40 @@
-fn main() {}
+mod shapes;
+
+use std::f64::consts::PI;
+
+use shapes::{Rect, Circle};
+
+
+trait Area {
+    fn area(&self) -> f64;
+}
+
+impl Area for Circle {
+    fn area(&self) -> f64 {
+        return self.radius * self.radius * PI;
+    }
+}
+
+impl Area for Rect {
+    fn area(&self) -> f64 {
+        return self.width * self.height;
+    }
+}
+
+fn main() {
+    let rect = Rect {
+        x: 0.0,
+        y: 0.0,
+        width: 10.0,
+        height: 10.0,
+    };
+
+    let circ = Circle {
+        x: 0.0,
+        y: 0.0,
+        radius: 10.0,
+    };
+
+    println!("{}" , circ.area());
+    println!("{}" , rect.area());
+}
---
+++ src/shapes.rs
@@ -0,0 +1,12 @@
+pub struct Rect {
+    pub x: f64,
+    pub y: f64,
+    pub width: f64,
+    pub height: f64,
+}
+
+pub struct Circle {
+    pub x: f64,
+    pub y: f64,
+    pub radius: f64,
+}
