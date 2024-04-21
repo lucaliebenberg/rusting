20889cda0a49a58932a221e52d77fea44b2dd785
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-04-21 12:56:00 +0200

    updates from building Iterator using impl and Struct

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,4 @@
-mod shapes;
-
-use crate::shapes::{rect::Rect, circle::Circle, area::Area};
-
 fn main() {
-    let rect = Rect {
-        x: 0.0,
-        y: 0.0,
-        width: 10.0,
-        height: 10.0,
-    };
+    /* excutor file */
 
-    let circ = Circle {
-        x: 0.0,
-        y: 0.0,
-        radius: 10.0,
-    };
-
-    println!("{}" , circ.area());
-    println!("{}" , rect.area());
-    println!("{}" , 6.9.area());
 }
--- src/shapes/rect.rs
+++ src/shapes/rect.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use super:: area::Area;
 
 pub struct Rect {
@@ -10,4 +12,64 @@ impl Area for Rect {
     fn area(&self) -> f64 {
         return self.width * self.height;
     }
+}
+
+impl Default for Rect {
+    fn default() -> Self {
+        return Rect {
+            x: 0.0,
+            y: 0.0,
+            width: 10.0,
+            height: 10.0,
+        };
+    }
+}
+
+impl Display for Rect {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        return write!(
+            f, 
+            "Rectangle({}, {}): {}x{}",
+         self.x,
+         self.y,
+         self.width,
+         self.height
+        );
+    }
+}
+
+pub struct RectIter {
+    points: Vec<(f64, f64)>,
+    idx: usize,
+}
+
+impl Iterator for RectIter {
+    type Item = (f64, f64); 
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let idx = self.idx;
+        self.idx += 1;
+
+        return self.points.get(idx)
+            .map(|x| * x);
+    }
+}
+
+impl IntoIterator for Rect {
+    type Item = (f64, f64); 
+
+    type IntoIterator = RectIter;
+
+    fn into_iter(self) -> Self::IntoIterator {
+        return RectIter {
+            points: vec![
+                (self.x, self.y),
+                (self.x, self.width, self.y),
+                (self.x, self.y, self.height),
+                (self.x + width, self.y + self.height),               
+            ],
+            idx: 0,
+        };
+    }
+
 }
